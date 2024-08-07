3946abd694f22cdf3867f2bc4270bb34d5d3a363
IgorAugust0 <augustoigor269@gmail.com>
2024-08-07 16:19:34 -0300

    feat: product-api category GET/search params * for some reason its necessary to use the 'value' property from PathVariable if using VS Code, tried adjust the build.gradle but it didnt work, while it ended up working in intellij

--- products-api/src/main/java/com/igor/microcomms/products_api/modules/category/repository/CategoryRepository.java
+++ products-api/src/main/java/com/igor/microcomms/products_api/modules/category/repository/CategoryRepository.java
@@ -2,6 +2,9 @@ package com.igor.microcomms.products_api.modules.category.repository;
 
 import com.igor.microcomms.products_api.modules.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    // SELECT * FROM category WHERE description = ?
+    List<Category> findByDescriptionIgnoreCaseContaining(String description);
 }
--- products-api/src/main/java/com/igor/microcomms/products_api/modules/category/service/CategoryService.java
+++ products-api/src/main/java/com/igor/microcomms/products_api/modules/category/service/CategoryService.java
@@ -7,6 +7,7 @@ import com.igor.microcomms.products_api.modules.category.model.Category;
 import com.igor.microcomms.products_api.modules.category.repository.CategoryRepository;
 
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 import static org.springframework.util.ObjectUtils.isEmpty;
 
@@ -19,7 +20,33 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    public CategoryResponse findByIdResponse(Integer id) {
+        return CategoryResponse.of(findById(id));
+    }
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository
+                .findAll()
+                .stream() // convert Category to CategoryResponse
+                .map(CategoryResponse::of) // apply CategoryResponse.of to each Category
+                .toList(); // convert Stream to List
+    }
+
+    public List<CategoryResponse> findByDescription(String description) {
+        if (isEmpty(description)) {
+            throw new ValidationException("Category description is required");
+        }
+        return categoryRepository
+                .findByDescriptionIgnoreCaseContaining(description)
+                .stream()
+                .map(CategoryResponse::of)
+                .toList();
+    }
+
     public Category findById(Integer id) {
+        if (isEmpty(id)) {
+            throw new ValidationException("Category id is required");
+        }
         return categoryRepository
                 .findById(id)
                 .orElseThrow(() -> new ValidationException("Category not found"));
