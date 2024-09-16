c185dcf79a5cfd321dd15654ca5c4e9515fb7f0d
IgorAugust0 <augustoigor269@gmail.com>
2024-09-16 17:46:50 -0300

    feat(sales-api): add placeholder data generation script
    - Created a script to generate placeholder data for testing and development purposes

---
+++ sales-api/src/lib/placeholder-data.ts
@@ -0,0 +1,52 @@
+import Order from "../modules/sales/model/order.ts";
+
+export async function createPlaceholderData() {
+  await Order.collection.drop();
+  await Order.create({
+    products: [
+      {
+        productId: 1,
+        quantity: 2,
+      },
+      {
+        productId: 2,
+        quantity: 1,
+      },
+      {
+        productId: 3,
+        quantity: 3,
+      },
+    ],
+    user: {
+      id: "a8s7a9d8a9s8d7a9s8d7",
+      name: "John Doe",
+      email: "johndoe@email.com",
+    },
+    status: "APPROVED",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  await Order.create({
+    products: [
+      {
+        productId: 4,
+        quantity: 1,
+      },
+      {
+        productId: 5,
+        quantity: 2,
+      },
+    ],
+    user: {
+      id: "j2da9s8d9a8s7d9a8s7d",
+      name: "Jane Doe",
+      email: "janedoe@email.com",
+    },
+    status: "REJECTED",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  console.log("Placeholder data created");
+  const orders = await Order.find();
+  console.log("Orders:", orders);
+}
--- sales-api/src/server.ts
+++ sales-api/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { connectToMongoDB } from "./config/db/mongo-db-config.ts";
-import Order from "./modules/sales/model/order";
+import { createPlaceholderData } from "./lib/placeholder-data.ts";
 
 const app = express();
 const env = process.env.NODE_ENV || "development";
@@ -8,27 +8,18 @@ const port = process.env.PORT || 8082;
 
 async function startServer() {
   try {
-    await connectToMongoDB();
-
-    app.get("/api/status", async (req, res) => {
-      try {
-        const orders = await Order.find().lean().exec();
-        console.log("Orders:", orders);
-
-        res
-          .status(200)
-          .json({ service: "Sales-API", status: "up", httpStatus: 200 });
-      } catch (error) {
-        console.error("Error fetching orders:", error);
-        res
-          .status(500)
-          .json({ service: "Sales-API", status: "error", httpStatus: 500 });
-      }
+    app.get("/api/status", (req, res) => {
+      res
+        .status(200)
+        .json({ service: "Sales-API", status: "up", httpStatus: 200 });
     });
 
     app.listen(port, () => {
       console.log(`Server is running on port ${port} in ${env} mode`);
     });
+
+    await connectToMongoDB();
+    await createPlaceholderData();
   } catch (error) {
     console.error("Failed to start server:", error);
     process.exit(1);
