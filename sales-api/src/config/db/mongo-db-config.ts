dbee12c0edc75d9b580af22bfe476f91d7247039
IgorAugust0 <augustoigor269@gmail.com>
2024-09-16 16:01:18 -0300

    feat(sales-api): add Order model and improve server.ts
    - Created Order model for handling order data
    - Enhanced server.ts with improvements for better structure and performance

--- sales-api/src/config/db/mongo-db-config.ts
+++ sales-api/src/config/db/mongo-db-config.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { MONGO_DB_URL } from "../../lib/util.ts";
 
-export default async function connectToMongoDB() {
+export async function connectToMongoDB() {
   try {
     await mongoose.connect(MONGO_DB_URL);
     console.info("Connected to MongoDB");
@@ -10,5 +10,8 @@ export default async function connectToMongoDB() {
     process.exit(1);
   }
 
-  mongoose.connection.on("error", console.error.bind(console, "MongoDB connection error:"));
+  mongoose.connection.on(
+    "error",
+    console.error.bind(console, "MongoDB connection error:")
+  );
 }
--- sales-api/src/server.ts
+++ sales-api/src/server.ts
@@ -1,18 +1,38 @@
 import express from "express";
-import connectToMongoDB from "./config/db/mongo-db-config";
+import { connectToMongoDB } from "./config/db/mongo-db-config.ts";
+import Order from "./modules/sales/model/order";
 
 const app = express();
 const env = process.env.NODE_ENV || "development";
 const port = process.env.PORT || 8082;
 
-connectToMongoDB();
+async function startServer() {
+  try {
+    await connectToMongoDB();
 
-app.get("/api/status", (req, res) => {
-  return res
-    .status(200)
-    .json({ service: "Sales-API", status: "up", httpStatus: 200 });
-});
+    app.get("/api/status", async (req, res) => {
+      try {
+        const orders = await Order.find().lean().exec();
+        console.log("Orders:", orders);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} in ${env} mode`);
-});
+        res
+          .status(200)
+          .json({ service: "Sales-API", status: "up", httpStatus: 200 });
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+        res
+          .status(500)
+          .json({ service: "Sales-API", status: "error", httpStatus: 500 });
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port} in ${env} mode`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
